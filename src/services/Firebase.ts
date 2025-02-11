c0e77819e0a7fe6f7eba4125a12e62c189f44f23
Apolos666 <quangprox121@gmail.com>
2025-02-11 12:43:57 +0700

    feat: sucessfully intergrate with firebase authentication and firestore

---
+++ src/services/Firebase.ts
@@ -0,0 +1,118 @@
+﻿import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, User, signOut } from "firebase/auth";
+import { collection, doc, getDoc, getDocs, getFirestore, limit, limit, onSnapshot, orderBy, query, query, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+        const result = await signInWithPopup(auth, provider);
+        await new Promise(resolve => setTimeout(resolve, 100));
+        return result.user;
+    } catch (error) {
+        console.error("Error signing in with Google: ", error);
+        throw error;
+    }
+}
+
+export const getCurrentUser = (): Promise<User | null> =>  {
+    return new Promise((resolve) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            unsubscribe();
+            resolve(user);
+        })
+    })
+}
+
+export const signOutUser = async () => {
+    try {
+        await signOut(auth);
+    } catch (error) {
+        console.error("Error signing out: ", error);
+        throw error;
+    }
+}
+
+export const db = getFirestore(app);
+
+export const saveBestScore = async (userId: string, userName: string, score: number) => {
+    try {
+        await setDoc(doc(db, 'scores', userId), {
+            bestScore: score,
+            userName,
+            updatedAt: new Date().toISOString()
+        })
+    } catch (error) {
+        console.error("Error saving best score:", error);
+        throw error;
+    } 
+}
+
+export const getBestScore = async (userId: string): Promise<number> => {
+    try {
+        const docRef = doc(db, "scores", userId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            return docSnap.data().bestScore;
+        }
+
+        return 0;
+    } catch (error) {
+        console.error("Error getting best score:", error);
+        return 0;
+    }
+}
+
+export interface TopScore {
+    userId: string;
+    userName: string;
+    bestScore: number;
+    updatedAt: string;
+}
+
+export const getTopScores = async (limitCount: number = 10): Promise<TopScore[]> => {
+    try {
+        const scoresRef = collection(db, "scores");
+        const q = query(scoresRef, orderBy("bestScore", "desc"), limit(limitCount));
+        const querySnapshot = await getDocs(q);
+
+        return querySnapshot.docs.map(doc => ({
+            userId: doc.id,
+            ...doc.data()
+        } as TopScore)) 
+    } catch (error) {
+        console.error("Error getting top scores:", error);
+        return [];
+    }
+} 
+
+export const subscribeToTopScores = (limitCount: number = 10, onUpdate: (scores: TopScore[]) => void) => {
+    const scoresRef = collection(db, "scores");
+    const q = query(scoresRef, orderBy("bestScore", "desc"), limit(limitCount));
+
+    return onSnapshot(q, (snapshot) => {
+        const scores = snapshot.docs.map(doc => ({
+            userId: doc.id,
+            ...doc.data()
+        } as TopScore));
+        onUpdate(scores);
+    }, (error) => {
+        console.error("Error listening to top scores: ", error);
+    });
+}
--- src/ui/GameUI.ts
+++ src/ui/GameUI.ts
@@ -1,4 +1,6 @@
-﻿import {IGameScene} from "../scenes/Game.ts";
+﻿import {User} from "firebase/auth";
+import {IGameScene} from "../scenes/Game.ts";
+import {getBestScore, getCurrentUser, saveBestScore, signInWithGoogle, signOutUser, subscribeToTopScores, TopScore} from "../services/Firebase.ts"
 
 export class GameUI {
     private score: number = 0;
@@ -6,15 +8,34 @@ export class GameUI {
     private scene: IGameScene;
     private scoreTextContainer: Phaser.GameObjects.Container;
     private bestScoreTextContainer: Phaser.GameObjects.Container;
+    private signInContainer: Phaser.GameObjects.Container;
+    private userProfileContainer: Phaser.GameObjects.Container;
+    private signOutContainer: Phaser.GameObjects.Container;
+    private topScoreContainer: Phaser.GameObjects.Container;
     // @ts-ignore
     private newGameButtonContainer: Phaser.GameObjects.Container;
     private scorePointText: Phaser.GameObjects.BitmapText;
     private bestScorePointText: Phaser.GameObjects.BitmapText;
+    private currentUser: User | null = null;
 
     private static readonly BUTTON_BG_WIDTH = 100;
     private static readonly BUTTON_BG_HEIGHT = 40;
     private static readonly SCORE_BG_WIDTH = 100;
     private static readonly SCORE_BG_HEIGHT = 40;
+
+    private static readonly TOP_SCORES_COLORS = [
+        0xFFD700, // Gold
+    0xC0C0C0, // Silver
+    0xCD7F32, // Bronze
+    0x9370DB, // Purple
+    0x20B2AA, // Light Sea Green
+    0x4682B4, // Steel Blue
+    0xDA70D6, // Orchid
+    0x32CD32, // Lime Green
+    0xFF69B4, // Hot Pink
+    0x4169E1  // Royal Blue
+    ]
+    topScoresUnsubscribe: (() => void) | null = null;
 
     constructor(scene: IGameScene) {
         this.scene = scene;
@@ -25,6 +46,21 @@ export class GameUI {
         this.createNewGameButton();
         this.createScoreContainer();
         this.createBestScoreContainer();
+        this.checkAuthState();
+        this.createTopScoreContainer();
+    }
+
+    private async checkAuthState() {
+        const user = await getCurrentUser();
+        this.currentUser = user;
+        if (user) {
+            const savedBestScore = await getBestScore(user.uid);
+            this.bestScore = savedBestScore;
+            this.bestScorePointText?.setText(this.bestScore.toString());
+            this.createUserProfileContainer(user);
+        } else {
+            this.createSignInContainer();
+        }
     }
 
     private createNewGameButton() {
@@ -146,24 +182,222 @@ export class GameUI {
         this.updateBestScore(this.score);
     }
 
-    public updateBestScore(score: number) {
+    public async updateBestScore(score: number) {
+        const oldBestScore = this.bestScore;
         this.bestScore = Math.max(this.bestScore, score);
-        if (this.bestScore !== score) return;
+        
+        if (this.bestScore !== oldBestScore) {
+            // Chỉ lưu khi có best score mới
+            if (this.currentUser) {
+                await saveBestScore(this.currentUser.uid, this.currentUser.displayName!, this.bestScore);
+            }
 
-        const textPoint = this.scene.add
-            .bitmapText(GameUI.SCORE_BG_WIDTH / 2, 22, 'wendy', `${this.bestScore}`, 12)
-            .setOrigin(0.5)
-            .setTint(0x776e65);
-        this.bestScoreTextContainer.add(textPoint);
-        this.scene.tweens.add({
-            targets: textPoint,
-            y: { from: 22 + 20, to: 22 },
-            alpha: { from: 1, to: 0 },
-            duration: 800,
-            onComplete: () => {
-                textPoint.destroy();
+            const textPoint = this.scene.add
+                .bitmapText(GameUI.SCORE_BG_WIDTH / 2, 22, 'wendy', `${this.bestScore}`, 12)
+                .setOrigin(0.5)
+                .setTint(0x776e65);
+            this.bestScoreTextContainer.add(textPoint);
+            this.scene.tweens.add({
+                targets: textPoint,
+                y: { from: 22 + 20, to: 22 },
+                alpha: { from: 1, to: 0 },
+                duration: 800,
+                onComplete: () => {
+                    textPoint.destroy();
+                }
+            });
+            this.bestScorePointText.setText(this.bestScore.toString());
+        }
+    }
+
+    private createSignInContainer() {
+        const signInBg = this.scene.add.rectangle(0, 0, GameUI.BUTTON_BG_WIDTH, GameUI.BUTTON_BG_HEIGHT, 0x8f7a66)
+            .setInteractive()
+            .setOrigin(0)
+            .setStrokeStyle(0);
+        const buttonText = this.scene.add.bitmapText(
+            GameUI.BUTTON_BG_WIDTH / 2,
+            GameUI.BUTTON_BG_HEIGHT / 2,
+            'wendy',
+            'Sign In',
+            16
+        ).setOrigin(0.5, 0.5);
+
+        this.signInContainer = this.scene.add.container(
+            this.scene.width / 2 - this.scene.gameBgWidth / 2,
+            30,
+            [signInBg, buttonText]
+        );
+
+        signInBg.on('pointerover', () => {
+            signInBg.setFillStyle(0x9f8a76);
+            this.scene.game.canvas.style.cursor = 'pointer';
+        })
+        signInBg.on('pointerout', () => {
+            signInBg.setFillStyle(0x8f7a66);
+            this.scene.game.canvas.style.cursor = 'default';
+        })
+        signInBg.on('pointerdown', () => {
+            signInBg.setFillStyle(0x7f6a56);
+        })
+        signInBg.on('pointerup', async () => {
+            signInBg.setFillStyle(0x8f7a66);
+            try {
+                const user = await signInWithGoogle();
+                if (user) {
+                    this.createUserProfileContainer(user);
+                }
+            } catch (error) {
+                console.error("Failed to sign in:", error);
             }
         })
-        this.bestScorePointText.setText(this.bestScore.toString());
+    }
+
+    private async createUserProfileContainer(user: User) {
+        if (this.signInContainer)
+            this.signInContainer.destroy();
+
+        const savedBestScore = await getBestScore(user.uid);
+        this.bestScore = savedBestScore;
+        this.bestScorePointText?.setText(this.bestScore.toString());
+        this.currentUser = user;
+
+        const containerWidth = GameUI.BUTTON_BG_WIDTH;
+        const containerBg = this.scene.add.rectangle(0, 0, containerWidth, GameUI.BUTTON_BG_HEIGHT, 0x8f7a66)
+            .setOrigin(0)
+            .setStrokeStyle(0);
+
+        const displayName = user.displayName?.split(' ')[0] || 'User'; // Lấy first name hoặc mặc định
+        const nameText = this.scene.add.bitmapText(
+            25,
+            GameUI.BUTTON_BG_HEIGHT / 2,
+            'wendy',
+            displayName,
+            16
+        ).setOrigin(0, 0.5);
+
+        this.userProfileContainer = this.scene.add.container(
+            this.scene.width / 2 - this.scene.gameBgWidth / 2,
+            30,
+            [containerBg, nameText]
+        ); 
+
+        const signOutWidth = GameUI.BUTTON_BG_WIDTH;
+        const signOutBg = this.scene.add.rectangle(0, 0, signOutWidth, GameUI.BUTTON_BG_HEIGHT, 0x8f7a66)
+            .setInteractive()
+            .setOrigin(0)
+            .setStrokeStyle(0);
+
+        const signOutText = this.scene.add.bitmapText(
+            GameUI.BUTTON_BG_WIDTH / 2,
+            GameUI.BUTTON_BG_HEIGHT / 2,
+            'wendy',
+            'Sign out',
+            16
+        ).setOrigin(0.5);
+        
+        this.signOutContainer = this.scene.add.container(
+            this.scene.width / 2 - this.scene.gameBgWidth / 2 + containerWidth + 10,
+            30,
+            [signOutBg, signOutText]
+        );
+
+        signOutBg.on('pointerover', () => {
+            signOutBg.setFillStyle(0x9f8a76);
+            this.scene.game.canvas.style.cursor = 'pointer';
+        })
+        signOutBg.on('pointerout', () => {
+            signOutBg.setFillStyle(0x8f7a66);
+            this.scene.game.canvas.style.cursor = 'default';
+        })
+        signOutBg.on('pointerdown', () => {
+            signOutBg.setFillStyle(0x7f6a56);
+        })
+        signOutBg.on('pointerup', async () => {
+            signOutBg.setFillStyle(0x8f7a66);
+            try {
+                await signOutUser();
+                if (this.userProfileContainer) {
+                    this.userProfileContainer.destroy();
+                }
+                if (this.signOutContainer) {
+                    this.signOutContainer.destroy();
+                }
+                this.createSignInContainer();
+            } catch (error) {
+                console.error("Failed to sign in:", error);
+            }
+        })
+    }
+
+    private createTopScoreContainer() {
+        const containerWidth = 200;
+        const containerHeight = this.scene.gameBgHeight;
+        const containerX = this.scene.width / 2 - this.scene.gameBgWidth / 2 - containerWidth - 20;
+        const containerY = 150;
+
+        const bg = this.scene.add.rectangle(0, 0, containerWidth, containerHeight, 0xbbada0)
+            .setOrigin(0);
+
+        const titleText = this.scene.add.bitmapText(
+            containerWidth / 2,
+            20,
+            'wendy',
+            'TOP 10 PLAYERS',
+            20
+        ).setOrigin(0.5, 0);
+
+        this.topScoreContainer = this.scene.add.container(
+            containerX,
+            containerY,
+            [bg, titleText]
+        );
+
+        this.topScoresUnsubscribe = subscribeToTopScores(10, (scores) => {
+            this.updateTopScoresUI(scores);
+        })
+    }
+    
+    updateTopScoresUI(scores: TopScore[]) {
+        this.topScoreContainer.getAll().forEach((item, index) => {
+            if (index > 1) item.destroy();
+        })
+
+        scores.forEach((score, index) => {
+            const yPos = 60 + (index * 40);
+            const color = GameUI.TOP_SCORES_COLORS[index];
+
+            const rankText = this.scene.add.bitmapText(
+                20,
+                yPos,
+                'wendy',
+                `#${index + 1}`,
+                16
+            ).setTint(color);
+
+            const nameText = this.scene.add.bitmapText(
+                50,
+                yPos,
+                'wendy',
+                score.userName.split(' ')[0],
+                16
+            ).setTint(color);
+
+            const scoreText = this.scene.add.bitmapText(
+                180,
+                yPos,
+                'wendy',
+                score.bestScore.toString(),
+                16
+            ).setOrigin(1, 0).setTint(color);
+
+            this.topScoreContainer.add([rankText, nameText, scoreText]);
+        })
+    }
+
+    destroy() {
+        if (this.topScoresUnsubscribe) {
+            this.topScoresUnsubscribe();
+        }
     }
 }
